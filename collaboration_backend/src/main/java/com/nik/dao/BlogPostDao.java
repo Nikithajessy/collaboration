0b0b45dbfe29df7d31b4a602976db9907108ae07
HP <HP@192.168.1.13>
2018-02-21 19:18:49 +0530

    blog completed

--- collaboration_backend/src/main/java/com/nik/dao/BlogPostDao.java
+++ collaboration_backend/src/main/java/com/nik/dao/BlogPostDao.java
@@ -1,10 +1,12 @@
 package com.nik.dao;
 
+import java.util.List;
+
 import com.nik.model.BlogPost;
 
 public interface BlogPostDao {
 	
 	void addBlogPost(BlogPost blogPost);
-
-
+	List<BlogPost> getBlogs(boolean approved);
+	BlogPost getBlogById(int id);
 }
